918fa839af48b1b640e0c8d8e61b656c556cca07
Mark Lee <s30171@gmail.com>
2024-08-12 19:19:15 +0800

    add

---
+++ src/main/java/idv/mark/share_module/config/LogbackContextInitializer.java
@@ -0,0 +1,26 @@
+package idv.mark.share_module.config;
+
+
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertiesPropertySource;
+
+import java.util.Properties;
+
+public class LogbackContextInitializer implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
+
+    @Override
+    public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
+        ConfigurableEnvironment environment = event.getEnvironment();
+        String appName = environment.getProperty("spring.application.name");
+
+        Properties properties = new Properties();
+        properties.put("APP_NAME", appName);
+
+        MutablePropertySources propertySources = environment.getPropertySources();
+        propertySources.addFirst(new PropertiesPropertySource("logbackConfig", properties));
+    }
+}
+
