269b0fa5a161789dfdda6dd3bc1f5d326bfb34c3
Mark Lee <s30171@gmail.com>
2024-08-15 18:01:31 +0800

    add

--- src/main/java/idv/mark/share_module/config/ZipkinConfig.java
+++ src/main/java/idv/mark/share_module/config/ZipkinConfig.java
@@ -1,10 +1,16 @@
 package idv.mark.share_module.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+@Slf4j
 @Configuration
 @PropertySource("classpath:zipkin.yml")
 public class ZipkinConfig {
     // 讀取resource/zipkin.yml設定檔
+
+    public ZipkinConfig() {
+        log.info("ZipkinConfig init");
+    }
 }
