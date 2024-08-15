9755dece97358bd63872f179373176768ceb5281
Mark Lee <s30171@gmail.com>
2024-08-15 18:48:44 +0800

    add

--- src/main/java/idv/mark/share_module/config/ZipkinConfig.java
+++ src/main/java/idv/mark/share_module/config/ZipkinConfig.java
@@ -1,16 +1,36 @@
 package idv.mark.share_module.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @Slf4j
 @Configuration
-@PropertySource("classpath:zipkin.yml")
 public class ZipkinConfig {
     // 讀取resource/zipkin.yml設定檔
-
-    public ZipkinConfig() {
-        log.info("ZipkinConfig init");
+    @Bean
+    protected PropertySourcesPlaceholderConfigurer createPropertyConfigurer(ConfigurableEnvironment environment) {
+        PropertySourcesPlaceholderConfigurer result = new PropertySourcesPlaceholderConfigurer();
+        try {
+            Resource resource = new ClassPathResource("zipkin.properties");
+            result.setLocation(resource);
+            try (InputStream is = resource.getInputStream()) {
+                Properties properties = new Properties();
+                properties.load(is);
+                environment.getPropertySources().addFirst(new PropertiesPropertySource("zipkinProperties", properties));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 }
