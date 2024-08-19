c27e0e4f3bd0767036e2ccaf7aa7743c3ec1d2ac
Mark Lee <s30171@gmail.com>
2024-08-19 16:25:48 +0800

    add

---
+++ src/main/java/idv/mark/share_module/config/ConfigHelper.java
@@ -0,0 +1,21 @@
+package idv.mark.share_module.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConfigHelper implements ApplicationContextAware {
+
+    private static ApplicationContext context;
+
+    public static <T> T getBean(Class<T> clazz) {
+        return context.getBean(clazz);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
+    }
+}
