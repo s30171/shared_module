4c58e6a2c47ed24b80dbd6622dbc7ddb7e882108
Mark Lee <s30171@gmail.com>
2024-11-21 20:41:18 +0800

    add

--- src/main/java/idv/mark/share_module/QueueUtil.java
+++ src/main/java/idv/mark/share_module/QueueUtil.java
@@ -11,6 +11,7 @@ public class QueueUtil {
     private static final PriorityQueue<Model> queue = new PriorityQueue<>();
     private static Boolean stopped = false;
     private static Boolean busy = false;
+    private static Thread pollingThread;
 
     public static void add(String text) {
         Model model = new Model();
@@ -26,6 +27,16 @@ public class QueueUtil {
 
     public static void stop() {
         stopped = true;
+        if (pollingThread != null) {
+            while (busy) {
+                try {
+                    Thread.sleep(3000); // 短暫等待，降低 CPU 負擔
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            pollingThread.interrupt();
+        }
     }
 
     public static void go() {
@@ -37,27 +48,34 @@ public class QueueUtil {
     }
 
     public static void startPollingThread(AbstractMessageHandler handlerChain) {
-        new Thread(() -> {
-            while (!isStopped()) {
-                if (!busy && !queue.isEmpty()) {
-                    busy = true;
-                    try {
-                        Model model = queue.poll();
-                        if (model != null) {
-                            handlerChain.handle(model.getMessage());
+        if (pollingThread == null || !pollingThread.isAlive()) {
+            pollingThread = new Thread(() -> {
+                while (!isStopped()) {
+                    if (!busy && !queue.isEmpty()) {
+                        busy = true;
+                        try {
+                            Model model = queue.poll();
+                            if (model != null) {
+                                handlerChain.handle(model.getMessage());
+                            }
+                        } finally {
+                            synchronized (QueueUtil.class) {
+                                busy = false;
+                                QueueUtil.class.notify(); // 通知停止等待
+                            }
                         }
-                    } finally {
-                        busy = false;
+                    }
+
+                    try {
+                        Thread.sleep(5000); // 每5秒檢查一次
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        break; // 中斷時退出循環
                     }
                 }
-
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-            }
-        }).start();
+            });
+            pollingThread.start(); // 啟動輪詢執行緒
+        }
     }
 
     @Data
